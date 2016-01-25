6838385cc4e93d512e49e55ca5f8e4923066d164
Sebastian Faltoni <sebastian.faltoni@gmail.com>
2016-01-25 18:11:37 +0100

    splitting ihub in two sub interfaces

--- NukedBit.PubSub/IHub.cs
+++ NukedBit.PubSub/IHub.cs
@@ -2,9 +2,18 @@ using System.Threading.Tasks;
 
 namespace NukedBit.PubSub
 {
-    public interface IHub
+    public interface IHub : IPublisher, ISubscriber
+    {
+         
+    }
+
+    public interface IPublisher
     {
         Task Publish<T>(T message) where T : class;
+    }
+
+    public interface ISubscriber
+    {
         void Subscribe<T>(IHandleMessage<T> handleMessage) where T : class;
         void UnSubscribe<T>(IHandleMessage<T> handleMessage) where T : class;
     }
